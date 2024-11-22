29a9519d2901c4f395d436cbf2c517bc183829bc
IvanLugo1631 <ileanos@hgrinc.com>
2024-11-22 15:25:01 -0500

    [UPD][TEST] onboarding : verifying what it is better, if having a session in the backend, or  store the data in a local storage
    Signed-off-by: IvanLugo1631 <ileanos@hgrinc.com>

--- server.go
+++ server.go
@@ -1,48 +1,77 @@
 package main
 
 import (
-	"embed"
-	"html/template"
-	"log"
-	"net/http"
-	"onboarding/handlers"
+    "embed"
+    "html/template"
+    "log"
+    "net/http"
+    "onboarding/handlers"
+    "os"
+    
+    "github.com/gorilla/sessions"
+    "github.com/joho/godotenv"
 )
 
 //go:embed templates/*
 var templateFS embed.FS
 
+var store *sessions.CookieStore
+
 func main() {
-	// Parse templates
-	tpl, err := template.New("views").ParseFS(templateFS,
-		"templates/header.go.html",
-		"templates/footer.go.html",
-		"templates/navbar.go.html",
-		"templates/home.go.html",
-		"templates/progressbar.go.html",
-		"templates/pii.go.html",
-		"templates/signature.go.html",
-		"templates/employment.go.html",
-	)
-	if err != nil {
-		log.Fatalf("Error parsing templates: %v", err)
-	}
+    // Load environment variables from the .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 
-	// Set the template for handlers to use
-	handlers.SetTemplate(tpl)
+    // Get the session secret key from environment variables
+    secretKey := []byte(os.Getenv("SESSION_SECRET_KEY"))
+    store = sessions.NewCookieStore(secretKey)
+    
+    // Configure session options
+    store.Options = &sessions.Options{
+        Path:     "/",
+        HttpOnly: true,
+        MaxAge:   86400 * 1, // 1 day
+        Secure:   false,
+    }
 
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+    // Pass the session store to handlers
+    handlers.SetSessionStore(store)
 
-	// Define routes
-	http.HandleFunc("/", handlers.HandleHome)
-	http.HandleFunc("/personal-information", handlers.HandlePersonalInformation)
-	http.HandleFunc("/signature", handlers.HandleSignature)
-	http.HandleFunc("/employment", handlers.HandleEmployment)
+    // Parse templates
+    tpl, err := template.New("views").ParseFS(templateFS,
+        "templates/header.go.html",
+        "templates/footer.go.html",
+        "templates/navbar.go.html",
+        "templates/home.go.html",
+        "templates/progressbar.go.html",
+        "templates/pii.go.html",
+        "templates/signature.go.html",
+        "templates/employment.go.html",
+    )
+    if err != nil {
+        log.Fatalf("Error parsing templates: %v", err)
+    }
 
-	// Start the server
-	log.Println("Server started at http://0.0.0.0:8089")
-	log.Fatal(http.ListenAndServe(":8089", nil))
+    // Set the template for handlers to use
+    handlers.SetTemplate(tpl)
+
+    // Serve static files
+    fs := http.FileServer(http.Dir("./static"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    // Define routes
+    http.HandleFunc("/", handlers.HandleHome)
+    http.HandleFunc("/personal-information", handlers.HandlePersonalInformation)
+    http.HandleFunc("/signature", handlers.HandleSignature)
+    http.HandleFunc("/employment", handlers.HandleEmployment)
+
+    // Start the server
+    log.Println("Server started at http://0.0.0.0:8089")
+    log.Fatal(http.ListenAndServe(":8089", nil))
 }
+
 
 type BGOrderData struct {
 	Config Config `json:"config"`
